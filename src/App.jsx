ebe17d5a19e53bdeea313876681d71e9c59a6796
Eduardo Krummenauer <carloseduardokf@gmail.com>
2024-10-08 11:13:06 -0300

    Currency Converter

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ConverterForm from './components/ConverterForm'
+
+const App = () => {
+  return (
+    <div className='currency-conveter'>
+      <h2 className='converter-title'>App</h2>
+      <ConverterForm />
+    </div>
+
+
+  )
+}
+
+export default App
